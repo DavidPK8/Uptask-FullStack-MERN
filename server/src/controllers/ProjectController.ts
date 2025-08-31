62325812db8702152fbdf867654eb63f8881d528
DavidPK8 <DavidPK8@users.noreply.github.com>
2025-08-31 12:49:54 -0500

    Backend: Manejo de rutas y logica de CRUD para Proyectos

---
+++ server/src/controllers/ProjectController.ts
@@ -0,0 +1,75 @@
+import type { Request, Response } from "express";
+import Project from "../models/Proyect";
+
+export class ProjectController {
+    static createProject = async (req: Request, res: Response) => {
+        const project = new Project(req.body);
+
+        try {
+            await project.save();
+            res.status(201).send({ msg: "Project created successfully" });
+        } catch (error) {
+            res.status(500).send("There was a problem creating the project");
+        }
+    };
+
+    static getAllProjects = async (req: Request, res: Response) => {
+        try {
+            const projects = await Project.find({});
+            res.json({ projects });
+        } catch (error) {
+            res.status(500).send("There was a problem getting the projects");
+        }
+    };
+
+    static getProjectByID = async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        try {
+            const project = await Project.findById(id);
+
+            if (!project) {
+                const error = new Error("Project not found");
+                return res.status(404).send({ msg: error.message });
+            }
+
+            res.json({ project });
+        } catch (error) {
+            res.status(500).send("There was a problem getting the project");
+        }
+    };
+
+    static updateProject = async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        try {
+            const project = await Project.findByIdAndUpdate(id, req.body);
+
+            if (!project) {
+                const error = new Error("Project not found");
+                return res.status(404).send({ msg: error.message });
+            }
+
+            res.send({ msg: "Project updated successfully" });
+        } catch (error) {
+            res.status(500).send("There was a problem updating the project");
+        }
+    };
+
+    static deleteProject = async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        try {
+            const project = await Project.findByIdAndDelete(id);
+
+            if (!project) {
+                const error = new Error("Project not found");
+                return res.status(404).send({ msg: error.message });
+            }
+
+            res.send({ msg: "Project deleted successfully" });
+        } catch (error) {
+            res.status(500).send("There was a problem deleting the project");
+        }
+    };
+}
---
+++ server/src/routes/projectRoutes.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { body, param } from "express-validator";
+import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middlewares/validation";
+
+const router = Router();
+
+router.post(
+    "/",
+    body("projectName").notEmpty().withMessage("Project name is required"),
+    body("clientName").notEmpty().withMessage("Client name is required"),
+    body("description").notEmpty().withMessage("Description is required"),
+    handleInputErrors,
+    ProjectController.createProject
+);
+
+router.get("/", ProjectController.getAllProjects);
+router.get(
+    "/:id",
+    param("id").isMongoId().withMessage("Invalid Project ID"),
+    handleInputErrors,
+    ProjectController.getProjectByID
+);
+
+router.put(
+    "/:id",
+    param("id").isMongoId().withMessage("Invalid Project ID"),
+    body("projectName").notEmpty().withMessage("Project name is required"),
+    body("clientName").notEmpty().withMessage("Client name is required"),
+    body("description").notEmpty().withMessage("Description is required"),
+    handleInputErrors,
+    ProjectController.updateProject
+);
+
+router.delete(
+    "/:id",
+    param("id").isMongoId().withMessage("Invalid Project ID"),
+    handleInputErrors,
+    ProjectController.deleteProject
+);
+
+export default router;
