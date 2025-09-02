51ff248711b8bce8f21e11dc1dfa6a4a091cdd4b
DavidPK8 <DavidPK8@users.noreply.github.com>
2025-09-01 20:33:03 -0500

    Backend: Creando el Modelo para las Tareas

---
+++ server/src/models/Task.ts
@@ -0,0 +1,47 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+const taskStatus = {
+    PENDING: "pending",
+    ON_HOLD: "onHold",
+    IN_PROGRESS: "inProgress",
+    UNDER_REVIEW: "underReview",
+    COMPLETED: "completed",
+} as const;
+
+export type TaskStatus = (typeof taskStatus)[keyof typeof taskStatus];
+
+export interface ITask extends Document {
+    taskName: string;
+    description: string;
+    projectID: Types.ObjectId;
+    status: TaskStatus;
+}
+
+export const TaskSchema: Schema = new Schema(
+    {
+        taskName: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        description: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        projectID: {
+            type: Types.ObjectId,
+            ref: "Project",
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: Object.values(taskStatus),
+            default: taskStatus.PENDING,
+        },
+    },
+    { timestamps: true }
+);
+
+const Task = mongoose.model<ITask>("Task", TaskSchema);
+export default Task;
