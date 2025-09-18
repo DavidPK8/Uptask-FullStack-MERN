907c15869d507ce583cf189b23763481b76acfdd
DavidPK8 <DavidPK8@users.noreply.github.com>
2025-09-17 19:08:08 -0500

    Frontend: Creacion de Funcion para obtener un Proyecto por su ID

--- client/src/api/ProjectAPI.ts
+++ client/src/api/ProjectAPI.ts
@@ -1,4 +1,8 @@
-import { dashboardProjectSchema, type ProjectFormData } from "@/types/index";
+import {
+    dashboardProjectSchema,
+    type Project,
+    type ProjectFormData,
+} from "@/types/index";
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
 
@@ -21,6 +25,18 @@ export async function getProjects() {
         if (response.success) {
             return response.data;
         }
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function getProjectByID(id: Project["_id"]) {
+    try {
+        const { data } = await api(`/projects/${id}`);
+
+        return data.project;
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error);
--- client/src/views/projects/EditProjectView.tsx
+++ client/src/views/projects/EditProjectView.tsx
@@ -1,8 +1,16 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProjectByID } from "@/api/ProjectAPI";
 
 export default function EditProjectView() {
     const params = useParams();
     const projectID = params.projectID!;
+
+    const { data, isLoading, error, isError } = useQuery({
+        queryKey: ["editProject", projectID],
+        queryFn: () => getProjectByID(projectID),
+        retry: false,
+    });
 
     return <div>EditProjectView</div>;
 }
