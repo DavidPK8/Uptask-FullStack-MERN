dcb7014a4b17e246ae962f413b11916993aab3cb
DavidPK8 <DavidPK8@users.noreply.github.com>
2025-09-18 21:05:18 -0500

    Frontend: Creacion de Formulario para Editar Proyecto

--- client/src/views/projects/EditProjectView.tsx
+++ client/src/views/projects/EditProjectView.tsx
@@ -1,16 +1,27 @@
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getProjectByID } from "@/api/ProjectAPI";
+import EditProjectFrom from "@/components/project/EditProjectFrom";
 
 export default function EditProjectView() {
     const params = useParams();
     const projectID = params.projectID!;
 
-    const { data, isLoading, error, isError } = useQuery({
+    const { data, isLoading, isError } = useQuery({
         queryKey: ["editProject", projectID],
         queryFn: () => getProjectByID(projectID),
         retry: false,
     });
 
-    return <div>EditProjectView</div>;
+    if (isLoading) {
+        return <p className="text-5xl font-bold text-center">Cargando...</p>;
+    }
+
+    if (isError) {
+        return <Navigate to="/404"></Navigate>;
+    }
+
+    if (data) {
+        return <EditProjectFrom />;
+    }
 }
