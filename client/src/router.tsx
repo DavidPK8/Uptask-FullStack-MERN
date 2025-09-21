9ddc4cc8e8a33e4e3b7556d6df008563a8258a9f
DavidPK8 <DavidPK8@users.noreply.github.com>
2025-09-21 13:58:12 -0500

    Frontend: Creando ruta para ver los Detalles del Proyecto

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,6 +3,7 @@ import AppLayout from "@/layouts/AppLayout";
 import DashboardView from "@/views/DashboardView";
 import CreateProjectView from "./views/projects/CreateProjectView";
 import EditProjectView from "./views/projects/EditProjectView";
+import ProjectDetailsView from "./views/projects/ProjectDetailsView";
 
 export default function Router() {
     return (
@@ -13,6 +14,10 @@ export default function Router() {
                     <Route
                         path="/projects/create"
                         element={<CreateProjectView />}
+                    />
+                    <Route
+                        path="/projects/:projectID"
+                        element={<ProjectDetailsView />}
                     />
                     <Route
                         path="/projects/:projectID/edit"
