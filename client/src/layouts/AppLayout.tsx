801afb7e127ca8ccbf511ebab058daa6865f10f0
DavidPK8 <DavidPK8@users.noreply.github.com>
2025-09-21 18:41:01 -0500

    Frontend: Creando Modal para implementar una Tarea Nueva

--- client/src/layouts/AppLayout.tsx
+++ client/src/layouts/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { Slide, ToastContainer } from "react-toastify";
 import Logo from "@/components/Logo";
 import NavMenu from "@/components/NavMenu";
@@ -9,7 +9,9 @@ export default function AppLayout() {
             <header className="bg-gray-800 py-5">
                 <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
                     <div className="w-64">
-                        <Logo />
+                        <Link to={"/"}>
+                            <Logo />
+                        </Link>
                     </div>
 
                     <NavMenu />
@@ -26,7 +28,11 @@ export default function AppLayout() {
                 </p>
             </footer>
 
-            <ToastContainer pauseOnHover={false} transition={Slide} autoClose={2500}/>
+            <ToastContainer
+                pauseOnHover={false}
+                transition={Slide}
+                autoClose={2500}
+            />
         </>
     );
 }
--- client/src/views/projects/ProjectDetailsView.tsx
+++ client/src/views/projects/ProjectDetailsView.tsx
@@ -1,5 +1,56 @@
+import {
+    Navigate,
+    useLocation,
+    useNavigate,
+    useParams,
+} from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProjectByID } from "@/api/ProjectAPI";
+import AddTaskModal from "@/components/task/AddTaskModal";
+
 export default function ProjectDetailsView() {
-  return (
-    <div>ProjectDetailsView</div>
-  )
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const params = useParams();
+    const projectID = params.projectID!;
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["editProject", projectID],
+        queryFn: () => getProjectByID(projectID),
+        retry: false,
+    });
+
+    if (isLoading) {
+        return <p className="text-5xl font-bold text-center">Cargando...</p>;
+    }
+
+    if (isError) {
+        return <Navigate to="/404"></Navigate>;
+    }
+
+    if (data) {
+        return (
+            <>
+                <h1 className="text-5xl font-black">{data.projectName}</h1>
+                <p className="text-2xl font-light text-gray-500 mt-5">
+                    {data.description}
+                </p>
+
+                <nav className="my-5 flex gap-3">
+                    <button
+                        type="button"
+                        className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+                        onClick={() =>
+                            navigate(location.pathname + "?newtask=true")
+                        }
+                    >
+                        Agregar Tarea
+                    </button>
+                </nav>
+
+                <AddTaskModal />
+            </>
+        );
+    }
 }
