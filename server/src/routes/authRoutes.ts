e64c236b03b91d6705faf764b6597e90485e4b0d
DavidPK8 <DavidPK8@users.noreply.github.com>
2025-10-03 18:14:48 -0500

    Backend: Manejo de errores al crear una Cuenta

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,9 +1,26 @@
 import { Router } from "express";
+import { AuthController } from "../controllers/AuthController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middlewares/validation";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Desde /api/auth");
-});
+router.post(
+    "/",
+    body("userName").notEmpty().withMessage("UserName is Required"),
+    body("password")
+        .isLength({ min: 8 })
+        .withMessage("Password is short, minimum of 8 characters"),
+    body("passwordConfirmation").custom((value, {req}) => {
+        if (value !== req.body.password) {
+            throw new Error("Passwords are not the same")
+        }
+
+        return true;
+    }),
+    body("email").isEmail().withMessage("Invalid Email"),
+    handleInputErrors,
+    AuthController.createAccount
+);
 
 export default router;
