449518921e956f8578de72a2db7f3df29b2e6602
DavidPK8 <DavidPK8@users.noreply.github.com>
2025-10-04 15:18:50 -0500

    Backend: Validando la respuesta al crear una cuenta

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express";
+import User from "../models/User";
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
-        res.send("Desde /api/auth");
+        try {
+            const user = new User(req.body);
+            await user.save();
+
+            res.json({ msg: "Account created, check your email to confirm" });
+        } catch (error) {
+            res.status(500).json({ error: "There was an error" });
+        }
     };
 }
