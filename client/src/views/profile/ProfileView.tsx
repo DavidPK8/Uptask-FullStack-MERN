5dd6bdb1788da94e82121c4d10e9175c93a6b3a9
DavidPK8 <DavidPK8@users.noreply.github.com>
2025-10-30 16:47:21 -0500

    Frontend: Creando formulario para cambiar el perfil del Usuario

--- client/src/views/profile/ProfileView.tsx
+++ client/src/views/profile/ProfileView.tsx
@@ -1,5 +1,14 @@
+import ProfileForm from "@/components/profile/ProfileForm";
+import { useAuth } from "@/hooks/useAuth";
+
 export default function ProfileView() {
-  return (
-    <div>ProfileView</div>
-  )
+    const { data, isLoading } = useAuth();
+
+    if (isLoading) {
+        return <p className="text-5xl font-bold text-center">Cargando...</p>;
+    }
+
+    if (data) {
+        return <ProfileForm data={data} />;
+    }
 }
