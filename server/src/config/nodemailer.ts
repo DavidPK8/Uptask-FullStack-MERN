b23576ed155ca372062849541695fbbc8db80975
DavidPK8 <DavidPK8@users.noreply.github.com>
2025-11-18 20:10:59 -0500

    Backend: Solucionando error para enviar Emails

--- server/src/config/nodemailer.ts
+++ server/src/config/nodemailer.ts
@@ -5,7 +5,9 @@ dotenv.config();
 
 const config = () => {
     return {
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
